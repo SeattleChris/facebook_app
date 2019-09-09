5168053107c1a876d6fe4c677716e8060fe76296
Chris L Chapman <ChristopherLChapman42@gmail.com>
2019-09-09 14:35:13 -0700

    Setup files

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 # For google cloud: first it looks for 'entrypoint' in app.yaml, then for 'app' here
 app = Flask(__name__)
@@ -8,13 +8,19 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return render_template('hello.html', name='Chris')
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     """ Return our home index page """
-    
+    return render_template('home.html')
+
+
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
+
 
 if __name__ == '__main__':
     # Used for running locally. When deployed on Google App Engine, a webserver
