1cdd5c5254b7ab46a98c76f403fe14ffd529c6bd
Chris L Chapman <ChristopherLChapman42@gmail.com>
2019-10-02 23:55:59 -0700

    Scaffold user model, Flask session cookies

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-
-from flask import Flask, request, render_template
-import facebook
+from facebook import get_user_from_cookie, GraphAPI
+from flask import Flask, g, render_template, redirect, request, session, url_for
+# from flask.ext.sqlalchemy import SQLAlchemy
+# import requests
+from dotenv import load_dotenv
 import os
+# from app import app, db
+from .models import User
 
 
 # For google cloud: first it looks for 'entrypoint' in app.yaml, then for 'app' here
+load_dotenv()
 app = Flask(__name__)
+# app.config.from_object("config")
+# db = SQLAlchemy(app)
+fb_app_name = os.getenv('FACEBOOK_APP_NAME', '')
+fb_app_id = os.getenv('FACEBOOK_APP_ID', '')
+fb_app_secret = os.getenv('FACEBOOK_APP_SECRET', '')
+fb_api_ver = os.getenv('FACEBOOK_API_VERSION', '3.1')
+# graph = GraphAPI(access_token=fb_app_secret, version=fb_api_ver)
+perms = [
+    'email',
+    'user_link',
+    'user_friends',
+    'user_likes',
+    'user_photos',
+    'user_posts',
+    'user_tagged_places',
+    'user_videos',
+    'groups_access_member_info',
+    'pages_show_list',
+    'read_insights',
+    'instagram_basic',
+    'instagram_manage_comments',
+    'instagram_manage_insights',
+        ]
 
 
 @app.route('/hello')
@@ -14,51 +42,136 @@ def hello():
     return render_template('hello.html', name='Chris')
 
 
-@app.route('/join')
-def join():
-    """Add a user for our app"""
-    # DEPLOYED_SITE = os.environ.get('DEPLOYED_SITE', '')
-    confirm_url = 'https://fb-test-251219.appspot.com/confirm'  # refactor with variables
-    fb_app_id = os.environ.get('FACEBOOK_APP_ID', '')
-    fb_app_token = os.environ.get('FACEBOOK_APP_SECRET', '')
-    fb_api_ver = os.environ.get('FACEBOOK_API_VERSION', '3.1')
-    graph = facebook.GraphAPI(access_token=fb_app_token, version=fb_api_ver)
-    perms = [
-        'default',
-        'email',
-        'user_link',
-        'user_friends',
-        'user_likes',
-        'user_photos',
-        'user_posts',
-        'user_tagged_places',
-        'user_videos',
-        'groups_access_member_info',
-        'pages_show_list',
-        'read_insights',
-        'instagram_basic',
-        'instagram_manage_comments',
-        'instagram_manage_insights',
-            ]
-    fb_login_url = graph.get_auth_url(fb_app_id, confirm_url, perms)
-    print('====================================================')
-    print(fb_login_url)
-    print('====================================================')
-    return render_template('confirm.html')
+@app.route("/")
+def index():
+    # If a user was set in the get_current_user function before the request,
+    # the user is logged in.
+    if g.user:
+        return render_template(
+            "index.html", app_id=fb_app_id, app_name=fb_app_name, user=g.user
+        )
+    # Otherwise, a user is not logged in.
+    return render_template("login.html", app_id=fb_app_id, name=fb_app_name)
 
 
-@app.route('/confirm')
-def confirm():
-    """ Where user is sent after confirming app permissions """
-    print('====== Confirm ========')
-    return render_template('confirm.html')
+@app.route("/logout")
+def logout():
+    """Log out the user from the application.
+    Log out the user from the application by removing them from the
+    session.  Note: this does not log the user out of Facebook - this is done
+    by the JavaScript SDK.
+    """
+    session.pop("user", None)
+    return redirect(url_for("index"))
 
 
+@app.before_request
+def get_current_user():
+    """Set g.user to the currently logged in user.
+    Called before each request, get_current_user sets the global g.user
+    variable to the currently logged in user.  A currently logged in user is
+    determined by seeing if it exists in Flask's session dictionary.
+    If it is the first time the user is logging into this application it will
+    create the user and insert it into the database.  If the user is not logged
+    in, None will be set to g.user.
+    """
 
-@app.route('/', methods=['GET'])
-def home():
-    """ Return our home index page """
-    return render_template('home.html')
+    # Set the user in the session dictionary as a global g.user and bail out
+    # of this function early.
+    if session.get("user"):
+        g.user = session.get("user")
+        return
+
+    # Attempt to get the short term access token for the current user.
+    result = get_user_from_cookie(
+        cookies=request.cookies, app_id=fb_app_id, app_secret=fb_app_secret
+    )
+
+    # If there is no result, we assume the user is not logged in.
+    if result:
+        # Check to see if this user is already in our database.
+        # user = User.query.filter(User.id == result["uid"]).first()
+        user = None
+
+        if not user:
+            # Not an existing user so get info
+            graph = GraphAPI(result["access_token"])
+            profile = graph.get_object("me")
+            if "link" not in profile:
+                profile["link"] = ""
+
+            # Create the user and insert it into the database
+            user = User(
+                id=str(profile["id"]),
+                name=profile["name"],
+                profile_url=profile["link"],
+                access_token=result["access_token"],
+            )
+            # db.session.add(user)
+        elif user.access_token != result["access_token"]:
+            # If an existing user, update the access token
+            user.access_token = result["access_token"]
+
+        # Add the user to the current session
+        session["user"] = dict(
+            name=user.name,
+            profile_url=user.profile_url,
+            id=user.id,
+            access_token=user.access_token,
+        )
+
+    # Commit changes to the database and set the user as a global g.user
+    # db.session.commit()
+    g.user = session.get("user", None)
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def join():
+#     """Add a user for our app"""
+#     # DEPLOYED_SITE = os.environ.get('DEPLOYED_SITE', '')
+#     confirm_url = 'https://fb-test-251219.appspot.com'  # refactor with variables
+#     fb_app_name = os.getenv('FACEBOOK_APP_NAME', '')
+#     fb_app_id = os.getenv('FACEBOOK_APP_ID', '')
+#     fb_app_secret = os.getenv('FACEBOOK_APP_SECRET', '')
+#     fb_api_ver = os.getenv('FACEBOOK_API_VERSION', '3.1')
+#     graph = GraphAPI(access_token=fb_app_secret, version=fb_api_ver)
+#     perms = [
+#         'email',
+#         'user_link',
+#         'user_friends',
+#         'user_likes',
+#         'user_photos',
+#         'user_posts',
+#         'user_tagged_places',
+#         'user_videos',
+#         'groups_access_member_info',
+#         'pages_show_list',
+#         'read_insights',
+#         'instagram_basic',
+#         'instagram_manage_comments',
+#         'instagram_manage_insights',
+#             ]
+#     fb_login_url = graph.get_auth_url(fb_app_id, confirm_url, perms)
+#     # response = requests.get(fb_login_url)
+#     # json_response = response.json()
+#     # has_token = False
+#     # if json_response['accessToken']:
+#     #     has_token = True
+#     # print(response.text)
+#     # return render_template('confirm.html', token_exists=response.text)
+#     return redirect(fb_login_url)
+
+# @app.route('/confirm')
+# def confirm():
+#     """ Where user is sent after confirming app permissions """
+#     print('====== Confirm ========')
+#     return render_template('confirm.html')
+
+
+# @app.route('/', methods=['GET'])
+# def home():
+#     """ Return our home index page """
+#     return render_template('home.html')
 
 
 @app.route('/<string:page_name>/')
